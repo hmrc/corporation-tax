7250277ef067219282a12d65e5ec3559561ab270
avelx <3528155+avelx@users.noreply.github.com>
2026-07-13 15:10:19 +0100

    DTR-6397: formatting: apply scalaFmt

--- app/uk/gov/hmrc/corporationtax/connectors/PenaltiesConnector.scala
+++ app/uk/gov/hmrc/corporationtax/connectors/PenaltiesConnector.scala
@@ -28,19 +28,17 @@ import java.net.URL
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
-class PenaltiesConnector @Inject()(http: HttpClientV2,
-                                   config: ServicesConfig)(implicit ec: ExecutionContext)
-  extends Logging {
+class PenaltiesConnector @Inject() (http: HttpClientV2, config: ServicesConfig)(implicit ec: ExecutionContext)
+    extends Logging {
 
   private val stubEnabled: Boolean = config.getBoolean("features.corporation-tax-stub-enabled")
 
-  private val dataProxyPath = {
+  private val dataProxyPath =
     if (stubEnabled) {
       config.baseUrl("corporation-tax-stub")
     } else {
       config.baseUrl("rds-datacache-proxy") + "/rds-datacache-proxy"
     }
-  }
 
   def getPenaltyTransactionList(taxRef: Long, accPeriod: Long)(implicit hc: HeaderCarrier): Future[Penalties] = {
     val url: URL = url"$dataProxyPath/corporation-tax/penalty-transactions/$taxRef/$accPeriod"
