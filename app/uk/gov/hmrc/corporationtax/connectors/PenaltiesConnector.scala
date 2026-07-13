245eabe74ff01ab5e4c607fd7745d183f219a0d1
avelx <3528155+avelx@users.noreply.github.com>
2026-07-13 11:49:46 +0100

    DTR-6397: fix: PenaltiesControllerSpec : address test / compile errors formatting: re-apply formatting across all scopes - [2]

--- app/uk/gov/hmrc/corporationtax/connectors/PenaltiesConnector.scala
+++ app/uk/gov/hmrc/corporationtax/connectors/PenaltiesConnector.scala
@@ -28,6 +28,7 @@ import java.net.URL
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
+//TODO: add stub integration/switch here
 class PenaltiesConnector @Inject() (http: HttpClientV2, config: ServicesConfig)(implicit ec: ExecutionContext)
     extends Logging {
 
