0dbbeac57d94601bea87474519c69284d7442a68
jowensoc <114502866+jowensoc@users.noreply.github.com>
2026-07-14 15:47:29 +0100

    adding new parts for Interest Accural List

---
+++ app/uk/gov/hmrc/corporationtax/controllers/InterestAccuralListController.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.corporationtax.controllers
+
+import play.api.Logging
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.corporationtax.connectors.InterestAccuralListConnector
+import uk.gov.hmrc.corporationtax.models.InterestAccuralList
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class InterestAccuralListController @Inject()(
+  cc: ControllerComponents,
+  interestAccuralListConnector: InterestAccuralListConnector
+)(implicit ec: ExecutionContext)
+    extends BackendController(cc)
+    with Logging {
+
+  def getInterestAccuralList(taxRef: Long, accPeriod: Long, interestType: String): Action[AnyContent] = Action.async { implicit request =>
+    interestAccuralListConnector
+      .getInterestAccuralList(taxRef, accPeriod, interestType)
+      .map { interestAccuralList =>
+        Ok(Json.toJson(interestAccuralList))
+      }
+      .recover { case ex: Exception =>
+        logger.error("Error while retrieving interest accural list", ex)
+        InternalServerError(Json.obj("error" -> "Failed to retrieve interest accural list"))
+      }
+  }
+
+}
---
+++ test/uk/gov/hmrc/corporationtax/controllers/InterestAccuralListControllerSpec.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.corporationtax.controllers
+
+import org.mockito.ArgumentMatchers.{any, eq as eqTo}
+import org.mockito.Mockito.{verify, when}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.mockito.MockitoSugar.mock
+import play.api.http.Status
+import play.api.libs.json.Json
+import play.api.mvc.Result
+import play.api.test.Helpers.*
+import play.api.test.{FakeRequest, Helpers}
+import uk.gov.hmrc.corporationtax.connectors.InterestAccuralListConnector
+import uk.gov.hmrc.corporationtax.helpers.InterestAccuralListHelper
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class InterestAccuralListControllerSpec extends AnyWordSpec with Matchers with InterestAccuralListHelper {
+
+  private trait Fixture {
+    val mockInterestAccuralListConnector: InterestAccuralListConnector = mock[InterestAccuralListConnector]
+
+    val cc                            = Helpers.stubControllerComponents()
+    implicit val ec: ExecutionContext = cc.executionContext
+    implicit val hc: HeaderCarrier    = HeaderCarrier()
+
+    val fakeRequest     = FakeRequest("GET", "/")
+    val fakePostRequest = FakeRequest("GET", "/WrongUrl")
+    val controller      = new InterestAccuralListController(Helpers.stubControllerComponents(), mockInterestAccuralListConnector)
+  }
+
+  "GET /" should {
+
+    "return 200: OK" in new Fixture {
+      when(mockInterestAccuralListConnector.getInterestAccuralList(any(), any(), any())(any[HeaderCarrier]))
+        .thenReturn(Future.successful(interestAccuralList))
+
+      val result: Future[Result] = controller.getInterestAccuralList(1, 2, "IDE")(fakeRequest)
+      status(result) shouldBe Status.OK
+
+      contentAsJson(result) shouldBe Json.toJson(interestAccuralList)
+
+      verify(mockInterestAccuralListConnector).getInterestAccuralList(eqTo(1L), eqTo(2L), eqTo("IDE"))(any[HeaderCarrier])
+    }
+
+    "return 500: INTERNAL_SERVER_ERROR" in new Fixture {
+      when(mockInterestAccuralListConnector.getInterestAccuralList(any(), any(), any())(any[HeaderCarrier]))
+        .thenReturn(Future.failed(new RuntimeException("unexpected")))
+
+      val result: Future[Result] = controller.getInterestAccuralList(1, 2, "IDE")(fakeRequest)
+      status(result)                               shouldBe Status.INTERNAL_SERVER_ERROR
+      (contentAsJson(result) \ "error").as[String] shouldBe "Failed to retrieve interest accural list"
+
+      verify(mockInterestAccuralListConnector).getInterestAccuralList(eqTo(1L), eqTo(2L), eqTo("IDE"))(any[HeaderCarrier])
+    }
+
+  }
+
+}
