5fc8d1f1f02ac763621f2e3732c4e16b1a94ed4a
jowensoc <114502866+jowensoc@users.noreply.github.com>
2026-07-16 13:45:07 +0100

    tweaks to test

--- test/uk/gov/hmrc/corporationtax/helpers/InterestAccuralListHelper.scala
+++ test/uk/gov/hmrc/corporationtax/helpers/InterestAccuralListHelper.scala
@@ -23,33 +23,33 @@ import java.time.LocalDate
 trait InterestAccuralListHelper {
 
   val interestAccuralList =
-    List(
-      InterestAccural(
-          computationAmount = 1,
-          interestAccrualFromDate = LocalDate.of(2021, 3, 7),
-          interestAccrualToDate = LocalDate.of(2021, 5, 7),
-          interestRate = 2,
-          interestAmount = 10,
-          apEndDate = LocalDate.of(2021, 6, 7)
-      ),
-      InterestAccural(
+    InterestAccuralList(
+      List(InterestAccural(
         computationAmount = 1,
-        interestAccrualFromDate = LocalDate.of(2023, 3, 7),
-        interestAccrualToDate = LocalDate.of(2023, 5, 7),
+        interestAccrualFromDate = LocalDate.of(2021, 3, 7),
+        interestAccrualToDate = LocalDate.of(2021, 5, 7),
         interestRate = 2,
         interestAmount = 10,
-        apEndDate = LocalDate.of(2023, 6, 7))
-    )
+        apEndDate = LocalDate.of(2021, 6, 7)
+      ),
+        InterestAccural(
+          computationAmount = 1,
+          interestAccrualFromDate = LocalDate.of(2023, 3, 7),
+          interestAccrualToDate = LocalDate.of(2023, 5, 7),
+          interestRate = 2,
+          interestAmount = 10,
+          apEndDate = LocalDate.of(2023, 6, 7))
+      ))
 
-  val interestAccuralSingleItemList = InterestAccuralList(
-    List(
-      InterestAccural(
+  val interestAccuralSingleItemList =
+    InterestAccuralList(
+      List(InterestAccural(
         computationAmount = 1,
         interestAccrualFromDate = LocalDate.of(2021, 3, 7),
         interestAccrualToDate = LocalDate.of(2021, 5, 7),
         interestRate = 2,
         interestAmount = 10,
         apEndDate = LocalDate.of(2021, 6, 7))
-    )
-  )
+      ))
+
 }
