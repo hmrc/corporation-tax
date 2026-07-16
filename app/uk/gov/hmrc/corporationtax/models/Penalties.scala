9b808fd83fa2234ac005c766e75b438f182da9db
avelx <3528155+avelx@users.noreply.github.com>
2026-07-16 15:47:41 +0100

    DTR-6397: feat: implement BF draft : convert PenaltyTransaction to PenaltyTransactionItem under isCPTF context; test: add test around Penalties.convertToItems method

--- app/uk/gov/hmrc/corporationtax/models/Penalties.scala
+++ app/uk/gov/hmrc/corporationtax/models/Penalties.scala
@@ -16,21 +16,71 @@
 
 package uk.gov.hmrc.corporationtax.models
 
-import play.api.libs.json.{Json, OFormat}
+import play.api.libs.json.*
+
 import java.time.LocalDate
 
 case class Penalties(penaltyTransactions: List[PenaltyTransaction])
 
 object Penalties {
   implicit val format: OFormat[Penalties] = Json.format[Penalties]
+
+  def convertToItems(penalty: PenaltyTransaction, isCTPF: Boolean): PenaltyTransactionItem = {
+    (penalty.`type`.toUpperCase(), isCTPF) match {
+      case ("F", true) =>
+        PenaltyTransactionItem(penaltyDate = penalty.penaltyDate, `type` = PenaltyTransactionType.FX, postingAmount = penalty.postingAmount)
+      case ("F", false) =>
+        PenaltyTransactionItem(penaltyDate = penalty.penaltyDate, `type` = PenaltyTransactionType.FT, postingAmount = penalty.postingAmount)
+      case (_, true) =>
+        PenaltyTransactionItem(penaltyDate = penalty.penaltyDate, `type` = PenaltyTransactionType.TG, postingAmount = penalty.postingAmount)
+      case (_, false) =>
+        PenaltyTransactionItem(penaltyDate = penalty.penaltyDate, `type` = PenaltyTransactionType.TR, postingAmount = penalty.postingAmount)
+    }
+  }
+
 }
 
 case class PenaltyTransaction(
-  penaltyDate: LocalDate,
-  `type`: String,
-  postingAmount: BigDecimal
-)
+                               penaltyDate: LocalDate,
+                               `type`: String,
+                               postingAmount: BigDecimal
+                             )
 
 object PenaltyTransaction {
   implicit val format: OFormat[PenaltyTransaction] = Json.format[PenaltyTransaction]
+}
+
+enum PenaltyTransactionType:
+  case FX, FT, TG, TR
+
+
+implicit val penaltyTransactionType: Format[PenaltyTransactionType] = new Format[PenaltyTransactionType] {
+  def reads(json: JsValue): JsResult[PenaltyTransactionType] = json match {
+    case JsString(s) => s.toLowerCase() match {
+      case "FX" => JsSuccess(PenaltyTransactionType.FX)
+      case "FT" => JsSuccess(PenaltyTransactionType.FT)
+      case "TG" => JsSuccess(PenaltyTransactionType.TG)
+      case "TR" => JsSuccess(PenaltyTransactionType.TR)
+      case _ => JsError("Invalid PenaltyTransactionType string")
+    }
+    case _ => JsError("Expected JsString")
+  }
+
+  def writes(penaltyType: PenaltyTransactionType): JsValue = JsString(penaltyType.toString.toUpperCase())
+}
+
+case class PenaltyTransactionItem(
+                                   penaltyDate: LocalDate,
+                                   `type`: PenaltyTransactionType,
+                                   postingAmount: BigDecimal
+                                 )
+
+object PenaltyTransactionItem {
+  implicit val format: OFormat[PenaltyTransactionItem] = Json.format[PenaltyTransactionItem]
+}
+
+case class PenaltyItems(penaltyTransactions: List[PenaltyTransactionItem])
+
+object PenaltyItems {
+  implicit val format: OFormat[PenaltyItems] = Json.format[PenaltyItems]
 }
