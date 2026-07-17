9bfcfcd7139de3a721f576652f54e4aed31c9f11
jack-brannon <166420733+Jack-Brannon@users.noreply.github.com>
2026-07-17 22:49:09 +0100

    DTR-6799 | Plugin update for coverage

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -15,7 +15,7 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[_]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 20,
+    ScoverageKeys.coverageMinimumStmtTotal := 70,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
