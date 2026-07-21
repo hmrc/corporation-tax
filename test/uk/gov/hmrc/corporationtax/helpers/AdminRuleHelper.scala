a9f5c93fe5ab7c7e3906561b8f0f3454d0232fa6
Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
2026-07-21 13:11:19 +0100

    DTR-6804:Developed GET adminRule for BE
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>

---
+++ test/uk/gov/hmrc/corporationtax/helpers/AdminRuleHelper.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.corporationtax.helpers
+
+import uk.gov.hmrc.corporationtax.models.AdminRule
+
+import java.time.LocalDate
+
+trait AdminRuleHelper {
+  val emptyAdminRule: AdminRule    = AdminRule(None, None)
+  val example1adminRule: AdminRule = AdminRule(Some(3L), Some(LocalDate.of(1997, 7, 1)))
+  val example2adminRule: AdminRule = AdminRule(Some(56L), Some(LocalDate.of(2001, 8, 12)))
+  val example3adminRule: AdminRule = AdminRule(Some(87L), None)
+  val example4adminRule: AdminRule = AdminRule(None, Some(LocalDate.of(2012, 4, 8)))
+}
