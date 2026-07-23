64133dc6a302f89fab21961cfef91272a3771584
Jack Brannon <166420733+Jack-Brannon@users.noreply.github.com>
2026-07-23 10:55:17 +0100

    Dtr 7239 (#16)
    * DTR-7239 | WIP, Initial commit for AccountingPeriodDetails integration
    
    * Tests added for accounting period details
    
    * DTR-7239 | Formatting

---
+++ app/uk/gov/hmrc/corporationtax/controllers/AccountingPeriodDetailsController.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.corporationtax.controllers
+
+import play.api.Logging
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.corporationtax.connectors.AccountingPeriodDetailsConnector
+import uk.gov.hmrc.corporationtax.utils.AmountAdjustableInstances.*
+import uk.gov.hmrc.corporationtax.utils.applyAmountTransform
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class AccountingPeriodDetailsController @Inject() (
+  cc: ControllerComponents,
+  connector: AccountingPeriodDetailsConnector
+)(implicit ec: ExecutionContext)
+    extends BackendController(cc)
+    with Logging {
+
+  def getAccountingPeriodDetails(taxRef: Long, accPeriod: Long): Action[AnyContent] = Action.async { implicit request =>
+    connector
+      .getAccountingPeriodDetails(taxRef, accPeriod)
+      .map { accountingPeriodDetails =>
+        Ok(
+          Json.toJson(applyAmountTransform(accountingPeriodDetails))
+        ) // TODO: BF 36 Boolean transformation needs adding still
+      }
+      .recover { case ex: Exception =>
+        logger.error("Error while retrieving tax transactions", ex)
+        InternalServerError(Json.obj("error" -> "Failed to retrieve accounting period details"))
+      }
+  }
+
+}
---
+++ test/uk/gov/hmrc/corporationtax/helpers/AccountingPeriodDetailsHelper.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.corporationtax.helpers
+
+import uk.gov.hmrc.corporationtax.models.AccountingPeriodDetails
+
+trait AccountingPeriodDetailsHelper {
+
+  val accountingPeriodDetails: AccountingPeriodDetails =
+    AccountingPeriodDetails(
+      isApBalanced = true,
+      lpiCalcFlag = true,
+      crDbCalcFlag = true,
+      creditInterestAmount = 123.235,
+      debitInterestAmount = 5930.02,
+      latePaymentInterestAmount = 3231.238,
+      repaymentInterestAmount = 1.231,
+      totalDerivedActualInterest = 2324.12,
+      amountDueForAp = 12.23
+    )
+
+  val accountingPeriodDetailsTransformedAmounts: AccountingPeriodDetails =
+    AccountingPeriodDetails(
+      isApBalanced = true,
+      lpiCalcFlag = true,
+      crDbCalcFlag = true,
+      creditInterestAmount = -123.24,
+      debitInterestAmount = -5930.02,
+      latePaymentInterestAmount = -3231.24,
+      repaymentInterestAmount = -1.23,
+      totalDerivedActualInterest = -2324.12,
+      amountDueForAp = -12.23
+    )
+
+}
