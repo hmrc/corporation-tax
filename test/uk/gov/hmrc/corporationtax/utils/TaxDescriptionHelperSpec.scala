ba32043e1da260048d852da57e487127f6cabe0f
jowensoc <114502866+jowensoc@users.noreply.github.com>
2026-07-31 13:31:01 +0100

    DTR-6378: Resolved spec test

--- test/uk/gov/hmrc/corporationtax/utils/TaxDescriptionHelperSpec.scala
+++ test/uk/gov/hmrc/corporationtax/utils/TaxDescriptionHelperSpec.scala
@@ -16,103 +16,349 @@
 
 package uk.gov.hmrc.corporationtax.utils
 
-import org.scalatest.Inspectors.forAll
 import org.scalatest.matchers.must.Matchers
-import org.scalatest.prop.Tables.Table
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.concurrent.ScalaFutures
 import play.api.i18n.{DefaultMessagesApi, Lang, Messages}
 
 class TaxDescriptionHelperSpec extends AnyWordSpec with Matchers with ScalaFutures {
 
-//  private val parametersTable = Table(
-//    ("A"),
-//    ("D"),
-//    ("E"),
-//    ("F"),
-//    ("J"),
-//    ("M"),
-//    ("R"),
-//    ("S"),
-//    ("T"),
-//    ("Z")
-//  )
-
-  private val parametersTable = Table(
-    ("A", "Amended assessment", "Amended assessment (claim)"),
-    ("D", "Discovery assessment", "Discovery assessment (claim)"),
-    ("E", "HMRC determination", "HMRC determination (claim)"),
-    ("F", "Further assessment", "Further assessment (claim)"),
-    ("J", "HMRC amended self assessment", "HMRC amended self assessment (claim)"),
-    ("M", "Main assessment", "Main assessment (claim)"),
-    ("R", "HMRC amended self assessment", "HMRC amended self assessment (claim)"),
-    ("S", "Self assessment", "Self assessment (claim)"),
-    ("T", "Taxpayer amended self assessment", "Taxpayer amended self assessment"),
-    ("Z", "Return charge", "Return charge (claim)")
-  )
-
   implicit val messages: Messages = new DefaultMessagesApi(
     Map(
       "en" -> Map(
         "taxDescription.assessment.a.standard" -> "Amended assessment",
-        "taxDescription.assessment.a.claim" -> "Amended assessment (claim)",
+        "taxDescription.assessment.a.claim"    -> "Amended assessment (claim)",
         "taxDescription.assessment.d.standard" -> "Discovery assessment",
-        "taxDescription.assessment.d.claim" -> "Discovery assessment (claim)",
+        "taxDescription.assessment.d.claim"    -> "Discovery assessment (claim)",
         "taxDescription.assessment.e.standard" -> "HMRC determination",
-        "taxDescription.assessment.e.claim" -> "HMRC determination (claim)",
+        "taxDescription.assessment.e.claim"    -> "HMRC determination (claim)",
         "taxDescription.assessment.f.standard" -> "Further assessment",
-        "taxDescription.assessment.f.claim" -> "Further assessment (claim)",
+        "taxDescription.assessment.f.claim"    -> "Further assessment (claim)",
         "taxDescription.assessment.j.standard" -> "HMRC amended self assessment",
-        "taxDescription.assessment.j.claim" -> "HMRC amended self assessment (claim)",
+        "taxDescription.assessment.j.claim"    -> "HMRC amended self assessment (claim)",
         "taxDescription.assessment.m.standard" -> "Main assessment",
-        "taxDescription.assessment.m.claim" -> "Main assessment (claim)",
+        "taxDescription.assessment.m.claim"    -> "Main assessment (claim)",
         "taxDescription.assessment.r.standard" -> "HMRC amended self assessment",
-        "taxDescription.assessment.r.claim" -> "HMRC amended self assessment (claim)",
+        "taxDescription.assessment.r.claim"    -> "HMRC amended self assessment (claim)",
         "taxDescription.assessment.s.standard" -> "Self assessment",
-        "taxDescription.assessment.s.claim" -> "Self assessment (claim)",
+        "taxDescription.assessment.s.claim"    -> "Self assessment (claim)",
         "taxDescription.assessment.t.standard" -> "Taxpayer amended self assessment",
-        "taxDescription.assessment.t.claim" -> "Taxpayer amended self assessment (claim)",
+        "taxDescription.assessment.t.claim"    -> "Taxpayer amended self assessment (claim)",
         "taxDescription.assessment.z.standard" -> "Return charge",
-        "taxDescription.assessment.z.claim" -> "Return charge (claim)",
+        "taxDescription.assessment.z.claim"    -> "Return charge (claim)"
       )
     )
   ).preferred(Seq(Lang("en")))
 
   private val taxDescriptionHelper = new TaxDescriptionHelper();
 
-  "TaxDescriptionHelper" should {
-    forAll(parametersTable) { assessmentType =>
-      s"Assessment Type is $assessmentType, it should return correct Tax Description" should {
+  s"Assessment Type is A, it should return correct Tax Description" should {
+    val assessmentType = "A"
 
-        "when Correction Claim Indicator is null" in {
-          val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, null).futureValue
+    "when Correction Claim Indicator is null" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, null).futureValue
 
-          val expectedResult = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
 
-          println(s"Correction Claim Null")
-          println(s"Actual: $result")
-          println(s"Expected: $expectedResult")
+      result mustBe expectedResult
+    }
 
-          result mustBe "dummy"
-        }
+    "when Correction Claim Indicator is 0" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "0").futureValue
 
-        "when Correction Claim Indicator is 0" in {
-          val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "0").futureValue
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
 
-          val expectedResult = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      result mustBe expectedResult
+    }
 
-          result mustBe expectedResult
-        }
+    "when Correction Claim Indicator is 2" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "2").futureValue
 
-        "when Correction Claim Indicator is 2" in {
-          val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "2").futureValue
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.claim"
+      val expectedResult = messages(messageName)
 
-          val expectedResult = s"taxDescription.assessment.${assessmentType.toLowerCase()}.claim"
-
-          result mustBe expectedResult
-        }
-      }
+      result mustBe expectedResult
     }
   }
 
+  s"Assessment Type is D, it should return correct Tax Description" should {
+    val assessmentType = "D"
+
+    "when Correction Claim Indicator is null" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, null).futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 0" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "0").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 2" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "2").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.claim"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+  }
+
+  s"Assessment Type is E, it should return correct Tax Description" should {
+    val assessmentType = "E"
+
+    "when Correction Claim Indicator is null" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, null).futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 0" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "0").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 2" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "2").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.claim"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+  }
+
+  s"Assessment Type is F, it should return correct Tax Description" should {
+    val assessmentType = "F"
+
+    "when Correction Claim Indicator is null" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, null).futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 0" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "0").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 2" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "2").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.claim"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+  }
+
+  s"Assessment Type is J, it should return correct Tax Description" should {
+    val assessmentType = "J"
+
+    "when Correction Claim Indicator is null" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, null).futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 0" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "0").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 2" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "2").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.claim"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+  }
+
+  s"Assessment Type is M, it should return correct Tax Description" should {
+    val assessmentType = "M"
+
+    "when Correction Claim Indicator is null" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, null).futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 0" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "0").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 2" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "2").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.claim"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+  }
+
+  s"Assessment Type is R, it should return correct Tax Description" should {
+    val assessmentType = "M"
+
+    "when Correction Claim Indicator is null" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, null).futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 0" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "0").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 2" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "2").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.claim"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+  }
+
+  s"Assessment Type is S, it should return correct Tax Description" should {
+    val assessmentType = "S"
+
+    "when Correction Claim Indicator is null" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, null).futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 0" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "0").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 2" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "2").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.claim"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+  }
+
+  s"Assessment Type is T, it should return correct Tax Description" should {
+    val assessmentType = "T"
+
+    "when Correction Claim Indicator is null" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, null).futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 0" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "0").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 2" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "2").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.claim"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+  }
+
+  s"Assessment Type is Z, it should return correct Tax Description" should {
+    val assessmentType = "Z"
+
+    "when Correction Claim Indicator is null" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, null).futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 0" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "0").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.standard"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+
+    "when Correction Claim Indicator is 2" in {
+      val result: String = taxDescriptionHelper.getTaxDescription(assessmentType, "2").futureValue
+
+      val messageName = s"taxDescription.assessment.${assessmentType.toLowerCase()}.claim"
+      val expectedResult = messages(messageName)
+
+      result mustBe expectedResult
+    }
+  }
 }
