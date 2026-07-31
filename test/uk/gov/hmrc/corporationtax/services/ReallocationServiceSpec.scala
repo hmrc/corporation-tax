7f094b5f515fc85315c413e9d478cfc33314617f
Pavel Oliynyk <3528155+avelx@users.noreply.github.com>
2026-07-31 15:19:47 +0100

    DTR-6397:  Accounting Periods I 38: GET Reallocations To Accounting Period (#20)
    * DTR-6397:
    feat: initial commit
    
    * DTR-6397:
    test: add basic test around service :=> ReallocationServiceSpec
    
    * DTR-6397:
    apply refactoring
    
    * DTR-6397:
    test: implement controller UT :: ReallocationControllerSpec
    apply: scalaFmt
    
    * DTR-6397:
    test: implement  required ISpec for connector and apply scalaFmt
    
    * DTR-6397:
    test: add connector failure test :: ReallocationServiceSpec
    
    * DTR-6397:
    test: address PR comment :: ReallocationControllerSpec
    
    * DTR-6397:
    test: address PR comment :: ReallocationControllerSpec
    
    * DTR-6397:
    test: apply standard amount transformation function
    
    * DTR-6397:
    test: fix failing test

---
+++ test/uk/gov/hmrc/corporationtax/services/ReallocationServiceSpec.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.corporationtax.services
+
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{verify, when}
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.mockito.MockitoSugar.mock
+import play.api.test.Helpers
+import uk.gov.hmrc.corporationtax.connectors.ReallocationsConnector
+import uk.gov.hmrc.corporationtax.helpers.ReallocationDataHelper
+import uk.gov.hmrc.corporationtax.models.Reallocations
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class ReallocationServiceSpec extends AnyWordSpec with Matchers with ScalaFutures with ReallocationDataHelper {
+
+  private trait Fixture {
+    val mockReallocationsConnector: ReallocationsConnector = mock[ReallocationsConnector]
+
+    val cc                            = Helpers.stubControllerComponents()
+    implicit val ec: ExecutionContext = cc.executionContext
+    implicit val hc: HeaderCarrier    = HeaderCarrier()
+
+    val service = new ReallocationService(mockReallocationsConnector)
+
+  }
+
+  "getByAccountingPeriod returns list of Reallocations from connector" in new Fixture {
+
+    when(mockReallocationsConnector.getByAccountingPeriod(any[Long], any[Long])(any[HeaderCarrier]))
+      .thenReturn(Future.successful(reallocationsTwoItems))
+
+    val result: Reallocations = service.getByAccountingPeriod(1L, 1L).futureValue
+
+    result shouldBe reallocationsExpected
+
+    verify(mockReallocationsConnector).getByAccountingPeriod(1L, 1L)(hc)
+  }
+
+  "getByAccountingPeriod returns failure from connector" in new Fixture {
+
+    val error = new RuntimeException("Simulate error")
+    when(mockReallocationsConnector.getByAccountingPeriod(any[Long], any[Long])(any[HeaderCarrier]))
+      .thenReturn(Future.failed(error))
+
+    val result: Throwable = service.getByAccountingPeriod(1L, 1L).failed.futureValue
+
+    result shouldBe error
+
+    verify(mockReallocationsConnector).getByAccountingPeriod(1L, 1L)(hc)
+  }
+
+}
