c617acd09d0914aa457e5a0ed6a7017566be8a3f
Roshan Gautam <106965552+Roshangautam2052@users.noreply.github.com>
2026-08-07 10:00:41 +0100

    DTR:7251: GET Reallocations From Accounting Period (#24)
    * DTR-7251:WIP
    
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
    
    * DTR-7251: GET Reallocations From Accounting Period
    
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
    
    * DTR-7251:Change ReallocationFromAccPeriod to match with DB_SCHEMA
    
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
    
    * DTR-7251:Create transformation logic for ReallocationFromAccPeriod to TransformedReallocationFromAccPeriod
    
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>

---
+++ app/uk/gov/hmrc/corporationtax/controllers/ReallocationFromAccPeriodController.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.corporationtax.controllers
+
+import play.api.Logging
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.corporationtax.services.ReallocationFromAccPeriodService
+import uk.gov.hmrc.http.UpstreamErrorResponse
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class ReallocationFromAccPeriodController @Inject() (
+  cc: ControllerComponents,
+  service: ReallocationFromAccPeriodService
+)(implicit ec: ExecutionContext)
+    extends BackendController(cc)
+    with Logging {
+
+  def getReallocationFromAccPeriod(taxPayerReference: Long, accPeriod: Long): Action[AnyContent] = Action.async {
+    implicit request =>
+      service
+        .getReallocationFromAccPeriod(taxPayerReference, accPeriod)
+        .map { reallocationFromAccPeriod =>
+          Ok(Json.toJson(reallocationFromAccPeriod))
+        }
+        .recover {
+          case u: UpstreamErrorResponse =>
+            Status(u.statusCode)(Json.obj("message" -> u.message))
+          case t: Throwable             =>
+            logger.error("Error while retrieving reallocationFromAccPeriod", t)
+            InternalServerError(Json.obj("error" -> "Failed to retrieve reallocationFromAccPeriod"))
+        }
+  }
+
+}
