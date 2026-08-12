63318a493e4bb0d9f91dd4b816984c7b74845eca
Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
2026-08-12 11:10:34 +0100

    DTR-7171:Implement Common Boolean Transformation
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>

--- app/uk/gov/hmrc/corporationtax/models/BusinessConstants.scala
+++ app/uk/gov/hmrc/corporationtax/models/BusinessConstants.scala
@@ -20,4 +20,7 @@ object BusinessConstants {
 
   val destinationTaxRefOASTransfer: String = "99"
 
+  val trueFlagSet: Set[String]  = Set("Y", "1")
+  val falseFlagSet: Set[String] = Set("N", "0")
+
 }
---
+++ app/uk/gov/hmrc/corporationtax/utils/CommonBooleanTransformation.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.corporationtax.utils
+import play.api.Logging
+import uk.gov.hmrc.corporationtax.models.BusinessConstants.*
+
+object CommonBooleanTransformation extends Logging {
+
+  // BF-F36 Boolean Common Transformation
+  def toBool(flag: String): Boolean =
+    val flagPreProcessed: String = flag.trim.toUpperCase
+    flagPreProcessed match {
+      case trueValue if trueFlagSet.contains(trueValue)    => true
+      case falseValue if falseFlagSet.contains(falseValue) => false
+      case _                                               =>
+        logger.warn(s"Unknown boolean value supplied <flag>: $flag")
+        false
+    }
+
+}
