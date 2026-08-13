4e3f26bfa3f72282ea86170f450ff34ba4bb45f8
Roshan Gautam <106965552+Roshangautam2052@users.noreply.github.com>
2026-08-13 14:56:50 +0100

    DTR-7171:Implement Common Boolean Transformation (#30)
    * DTR-7171:Implement Common Boolean Transformation
    
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
    
    * DTR-7171: Remove set of true and false values
    
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
    
    * DTR-7171:Remove logging from BooleanTransformation
    
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>

---
+++ test/uk/gov/hmrc/corporationtax/utils/CommonBooleanTransformationSpec.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.corporationtax.utils
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import uk.gov.hmrc.corporationtax.utils.CommonBooleanTransformation.toBool
+
+class CommonBooleanTransformationSpec extends AnyWordSpec with Matchers {
+
+  "CommonBooleanTransformation.toBool" should {
+
+    "return true when the input flag is Y" in {
+      toBool("Y") shouldBe true
+    }
+    "return true when the input flag is y" in {
+      toBool("y") shouldBe true
+    }
+    "return true when the input flag is 1" in {
+      toBool("1") shouldBe true
+    }
+
+    "return false when the input flag is N" in {
+      toBool("N") shouldBe false
+    }
+    "return false when the input flag is n" in {
+      toBool("n") shouldBe false
+    }
+
+    "return false when the input flag is 0" in {
+      toBool("0") shouldBe false
+    }
+
+    "trim spaces and return true when input flag is 1" in {
+      toBool("  1  ") shouldBe true
+    }
+
+    "return false when the input flag is unknown" in {
+      toBool("unknown") shouldBe false
+    }
+
+  }
+
+}
