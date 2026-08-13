048b9b3135c6ecfc3c15c67634cec0e3cfa6af13
Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
2026-08-13 14:32:53 +0100

    DTR-6405:WIP
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>

---
+++ app/uk/gov/hmrc/corporationtax/models/ReallocationToTransactionType.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.corporationtax.models
+
+import play.api.libs.json.*
+
+sealed trait TransactionTypesOfGetReallocationToAcc {
+  def value: String
+}
+
+case object MiscellaneousTransferGetReallocationFrom extends TransactionTypesOfGetReallocationToAcc {
+  override def value: String = "MiscTFR"
+}
+case object ReallocationFrom extends TransactionTypesOfGetReallocationToAcc {
+  override def value: String = "RFR"
+}
+
+object TransactionTypesOfGetReallocationToAcc {
+
+  val values: Seq[TransactionTypesOfGetReallocationToAcc] = Seq(MiscellaneousTransferGetReallocationFrom, ReallocationFrom)
+
+  private def fromString(value: String): Option[TransactionTypesOfGetReallocationToAcc] =
+    values.find(_.value == value)
+
+  implicit val reads: Reads[TransactionTypesOfGetReallocationToAcc] =
+    Reads {
+      case JsString(s) =>
+        fromString(s) match {
+          case Some(s) => JsSuccess(s)
+          case None    => JsError(s"Unknown TransactionTypesOfGetReallocationFromAcc: $s")
+        }
+
+      case unexpected => JsError(s"Expected JsString for TransactionTypesOfGetReallocationFromAcc, got: $unexpected")
+    }
+
+  implicit val writes: Writes[TransactionTypesOfGetReallocationToAcc] =
+    Writes(transactionType => JsString(transactionType.value))
+}
