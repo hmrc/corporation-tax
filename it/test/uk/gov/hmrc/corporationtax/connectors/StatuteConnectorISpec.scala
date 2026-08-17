8acff94cc3d7e908e7189090e37e54b2dc903aa9
avelx <3528155+avelx@users.noreply.github.com>
2026-08-17 15:20:26 +0100

    DTR-6385: test: basic IT test for: StatuteConnectorISpec

---
+++ it/test/uk/gov/hmrc/corporationtax/connectors/StatuteConnectorISpec.scala
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.corporationtax.connectors
+
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.http.Status.*
+import uk.gov.hmrc.corporationtax.itutils.ApplicationWithWiremock
+import uk.gov.hmrc.corporationtax.models.StatuteRule
+import uk.gov.hmrc.corporationtax.testdata.StatuteRuleHelper
+import uk.gov.hmrc.http.HeaderCarrier
+
+class StatuteConnectorISpec
+    extends AnyWordSpec
+    with Matchers
+    with ScalaFutures
+    with IntegrationPatience
+    with ApplicationWithWiremock
+    with BeforeAndAfterEach
+    with StatuteRuleHelper {
+
+  implicit val hc: HeaderCarrier = HeaderCarrier()
+
+  private val connector: StatuteRuleConnector = app.injector.instanceOf[StatuteRuleConnector]
+
+  "getStatueRule" should {
+
+    def url(ruleRateKey: String,
+            startDateStr: String, endDateStr: String) =
+      s"/rds-datacache-proxy/corporation-tax/statue-rule/$ruleRateKey/$startDateStr/$endDateStr"
+
+
+    "return no StatuteRule record" in {
+      stubFor(
+        get(urlPathEqualTo(url("C1", "1999-01-19", "1999-07-20")))
+          .willReturn(
+            aResponse()
+              .withStatus(NOT_FOUND)
+              .withBody(s"""{}""".stripMargin
+              )
+          )
+      )
+
+      val result = connector.getStatueRule("C1", "1999-01-19", "1999-07-20").futureValue
+      result mustBe None
+    }
+
+    "return StatuteRule default" in {
+      stubFor(
+        get(urlPathEqualTo(url("C1", "1999-01-19", "1999-07-20")))
+          .willReturn(
+            aResponse()
+              .withStatus(OK)
+              .withBody(
+                s"""{
+                   |  "statuteRule": {
+                   |    "numberOfDays":27,
+                   |    "ruleEndDate":"1999-02-14",
+                   |    "ruleAmount":100.011,
+                   |    "ruleRate":5.75,
+                   |    "ruleStartDate":"1999-01-18"
+                   |  }
+                   |}""".stripMargin
+              )
+          )
+      )
+
+      val result = connector.getStatueRule("C1", "1999-01-19", "1999-07-20").futureValue
+      result mustBe Some( StatuteRule(defaultRecord) )
+    }
+
+    "return StatuteRule record with empty fields" in {
+      stubFor(
+        get(urlPathEqualTo(url("C1", "1999-01-19", "1999-07-20")))
+          .willReturn(
+            aResponse()
+              .withStatus(OK)
+              .withBody(
+                s"""{
+                   |  "statuteRule": {
+
+                   |  }
+                   |}""".stripMargin
+              )
+          )
+      )
+
+      val result = connector.getStatueRule("C1", "1999-01-19", "1999-07-20").futureValue
+      result mustBe Some(StatuteRule(recordWithEmptyFields))
+    }
+
+    /*
+
+    "return Penalties list (two items) from BE with status code OK" in {
+      stubFor(
+        get(urlPathEqualTo(url(1L, 5L)))
+          .willReturn(
+            aResponse()
+              .withStatus(OK)
+              .withBody(
+                s"""{
+                   |"penaltyTransactions":
+                   |[
+                   |  {"penaltyDate":"2025-05-01","type":"F","postingAmount":100.13 },
+                   |  {"penaltyDate":"2021-03-07","type":"G","postingAmount":29.13 }
+                   |]}""".stripMargin
+              )
+          )
+      )
+
+      val result = connector.getPenaltyTransactionList(1L, 5L).futureValue
+      result.penaltyTransactions must contain allElementsOf penaltiesSingleItemList.penaltyTransactions
+    }
+
+    "return INTERNAL_ERROR when service failed" in {
+      stubFor(
+        get(urlPathEqualTo(url(1L, 5L)))
+          .willReturn(
+            aResponse()
+              .withStatus(INTERNAL_SERVER_ERROR)
+              .withBody(
+                s"""{
+                   |error" : "Failed to retrieve penalties"
+                   |}""".stripMargin
+              )
+          )
+      )
+
+      val ex = intercept[Exception] {
+        connector.getPenaltyTransactionList(1L, 5L).futureValue
+      }
+      ex.getMessage.toLowerCase must include("error")
+    }
+
+     */
+  }
+}
---
+++ it/test/uk/gov/hmrc/corporationtax/testdata/StatuteRuleHelper.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.corporationtax.testdata
+
+import uk.gov.hmrc.corporationtax.models.StatuteRuleItem
+
+import java.time.LocalDate
+
+trait StatuteRuleHelper {
+  val defaultRecord = StatuteRuleItem(
+    ruleStartDate = Some( LocalDate.parse("1999-01-18")),
+    ruleEndDate = Some( LocalDate.parse("1999-02-14")),
+    numberOfDays = Some(27),
+    ruleAmount = Some(BigDecimal(100.011)),
+    ruleRate = Some(BigDecimal(5.75))
+  )
+
+  val recordWithEmptyFields = StatuteRuleItem(
+    ruleStartDate = None,
+    ruleEndDate = None,
+    numberOfDays = None,
+    ruleAmount = None,
+    ruleRate = None
+  )
+}
