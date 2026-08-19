62fb988b4656573a936cac1b68ee95f13f5b5297
avelx <3528155+avelx@users.noreply.github.com>
2026-08-19 13:57:19 +0100

    DTR-6385: apply scalafmtAll

--- app/uk/gov/hmrc/corporationtax/connectors/StatuteRuleConnector.scala
+++ app/uk/gov/hmrc/corporationtax/connectors/StatuteRuleConnector.scala
@@ -43,7 +43,8 @@ class StatuteRuleConnector @Inject() (http: HttpClientV2, config: ServicesConfig
   def getStatueRule(ruleRateKey: String, startDateStr: String, endDateStr: String)(implicit
     hc: HeaderCarrier
   ): Future[Option[StatuteRule]] = {
-    val url: URL = url"$dataProxyPath/corporation-tax/statute-rule?ruleKey=$ruleRateKey&startDate=$startDateStr&endDate=$endDateStr"
+    val url: URL =
+      url"$dataProxyPath/corporation-tax/statute-rule?ruleKey=$ruleRateKey&startDate=$startDateStr&endDate=$endDateStr"
 
     http
       .get(url)
