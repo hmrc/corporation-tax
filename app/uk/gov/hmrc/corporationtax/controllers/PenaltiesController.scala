d97f6ac9f349f152939b210891cce2d13e1f9b76
avelx <3528155+avelx@users.noreply.github.com>
2026-08-21 11:23:03 +0100

    DTR-6385: address PR comments

--- app/uk/gov/hmrc/corporationtax/controllers/PenaltiesController.scala
+++ app/uk/gov/hmrc/corporationtax/controllers/PenaltiesController.scala
@@ -28,15 +28,15 @@ import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
 
-class PenaltiesController @Inject() (
-  cc: ControllerComponents,
-  service: PenaltiesService
-)(implicit ec: ExecutionContext)
-    extends BackendController(cc)
+class PenaltiesController @Inject()(
+                                     cc: ControllerComponents,
+                                     service: PenaltiesService
+                                   )(implicit ec: ExecutionContext)
+  extends BackendController(cc)
     with Logging {
 
   private def callPenaltiesService(taxRef: Long, accPeriod: Long, endDate: Option[LocalDate])(implicit
-    hc: HeaderCarrier
+                                                                                              hc: HeaderCarrier
   ): Future[Result] =
     service
       .getPenaltyTransactionList(taxRef, accPeriod, endDate)
@@ -49,25 +49,25 @@ class PenaltiesController @Inject() (
       }
 
   def getPenaltyTransactionList(
-    taxRef: Long,
-    accPeriod: Long,
-    endDateMaybe: Option[String]
-  ): Action[AnyContent] = Action.async { implicit request =>
+                                 taxRef: Long,
+                                 accPeriod: Long,
+                                 endDateMaybe: Option[String]
+                               ): Action[AnyContent] = Action.async { implicit request =>
     endDateMaybe match {
       case Some(endDateStr) =>
         Try {
           LocalDate.parse(endDateStr)
         }.toOption match {
           case Some(endDate) =>
-            callPenaltiesService(taxRef = taxRef, accPeriod = accPeriod, endDate = Some(endDate))
-          case None          =>
+            callPenaltiesService(taxRef, accPeriod, Some(endDate))
+          case None =>
             logger.error("Error while retrieving penalties: date conversion error")
             Future.successful {
               InternalServerError(Json.obj("error" -> "Failed to retrieve penalties: date conversion error"))
             }
         }
-      case None             =>
-        callPenaltiesService(taxRef = taxRef, accPeriod = accPeriod, endDate = None)
+      case None =>
+        callPenaltiesService(taxRef, accPeriod, None)
     }
 
   }
