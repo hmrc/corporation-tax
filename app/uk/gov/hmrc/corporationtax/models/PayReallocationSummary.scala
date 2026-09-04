0a3e06b576ccabb626c8b3b0a34bca842480b1e4
tioadesola1 <162135798+tioadesola1@users.noreply.github.com>
2026-09-04 09:19:30 +0100

    WIP

---
+++ app/uk/gov/hmrc/corporationtax/models/PayReallocationSummary.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.corporationtax.models
+
+import play.api.libs.json.{Json, OFormat}
+
+import java.time.LocalDate
+
+case class PayReallocationSummary(transactions: List[PayReallocationSummaryDetails])
+
+object PayReallocationSummary {
+  implicit val format: OFormat[PayReallocationSummary] = Json.format[PayReallocationSummary]
+}
+
+case class PayReallocationSummaryDetails(
+  transactionDate: Option[LocalDate],
+  `type`: Option[String],
+  amount: Option[BigDecimal],
+  accountingPeriodEndDate: Option[LocalDate],
+  taxpayerReference: Option[String],
+                                        
+)
+
+object PayReallocationSummaryDetails {
+  implicit val format: OFormat[PayReallocationSummaryDetails] = Json.format[PayReallocationSummaryDetails]
+}
