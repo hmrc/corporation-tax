738dbc7fc72dffd6a5ebbbeb5c8f9903d9556c9c
John Owens <114502866+jowensoc@users.noreply.github.com>
2026-09-15 16:28:40 +0100

    Dtr 6370 (#46)
    * DTR-6370 Added Display Needed services
    
    * DTR-6370 added Display Needed feature
    
    * DTR-6370 Rebased and updated spec tests. Removed incorrect setting in connector
    
    # Conflicts:
    #	conf/app.routes
    
    * DTR-6370 Formatted file
    
    * DTR-6370 Rebased main branch

---
+++ app/uk/gov/hmrc/corporationtax/connectors/DisplayNeededConnector.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.corporationtax.connectors
+
+import play.api.Logging
+import uk.gov.hmrc.*
+import uk.gov.hmrc.corporationtax.config.AppConfig
+import uk.gov.hmrc.corporationtax.models.DisplayNeededResponse
+import uk.gov.hmrc.http.HttpReads.Implicits.*
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.{HeaderCarrier, StringContextOps}
+
+import java.net.URL
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class DisplayNeededConnector @Inject() (http: HttpClientV2, appConfig: AppConfig)(implicit ec: ExecutionContext)
+    extends Logging {
+
+  def getDisplayNeeded(taxRef: Long, accPeriod: Long)(implicit hc: HeaderCarrier): Future[DisplayNeededResponse] = {
+    val url: URL = url"${appConfig.rdsDatacacheProxyFullUrl}/display-needed/$taxRef/$accPeriod"
+
+    http
+      .get(url)
+      .execute[DisplayNeededResponse]
+      .recover { case e: Throwable =>
+        logger.error(s"[DisplayNeededConnector][getDisplayNeeded]: $taxRef :: $accPeriod - ${e.getMessage}")
+        throw new RuntimeException(e.getMessage)
+      }
+  }
+
+}
