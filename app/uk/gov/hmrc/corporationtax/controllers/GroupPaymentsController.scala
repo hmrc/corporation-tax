fe3588981082547cbc1201ea521595e25c29f70f
avelx <3528155+avelx@users.noreply.github.com>
2026-09-24 11:53:24 +0100

    DTR-6433: feat: add require service

--- app/uk/gov/hmrc/corporationtax/controllers/GroupPaymentsController.scala
+++ app/uk/gov/hmrc/corporationtax/controllers/GroupPaymentsController.scala
@@ -19,7 +19,7 @@ package uk.gov.hmrc.corporationtax.controllers
 import play.api.Logging
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
-import uk.gov.hmrc.corporationtax.connectors.GroupPaymentsConnector
+import uk.gov.hmrc.corporationtax.services.GroupPaymentsService
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
 import javax.inject.Inject
@@ -27,7 +27,7 @@ import scala.concurrent.ExecutionContext
 
 class GroupPaymentsController @Inject() (
   cc: ControllerComponents,
-  connector: GroupPaymentsConnector
+  connector: GroupPaymentsService
 )(implicit ec: ExecutionContext)
     extends BackendController(cc)
     with Logging {
